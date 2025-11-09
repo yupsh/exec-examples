560a9b12c8f54130b5ce06f7dcdf2e081063c49f
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:20 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -1,17 +1,16 @@
 package exec_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/exec"
 )
 
 func ExampleExec_basic() {
 	// exec ls
 	// Note: This would execute the actual ls command
-	yup.MustRun(
+	gloo.MustRun(
 		Exec("ls"),
 	)
 	// Output:
 	//
 }
-
